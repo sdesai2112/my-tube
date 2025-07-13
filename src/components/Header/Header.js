6a748b04ec6c3faa4b7487d788bfbbd5e3a024e7
Shraddha <sshradhadesai@gmail.com>
2025-07-13 17:33:28 +0530

    Implemented debouncing for showing search suggestions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,35 @@
 import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
 import { toggleMenu } from "../../utils/store/slices/appSlice";
+import { API } from "../../utils/constants";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  useEffect(() => {
+    // Below is the implementation of Debouncing.
+    // API Call will be made after 200ms.
+    const timer = setTimeout(() => {
+      getSearchSuggestions();
+    }, 200);
+
+    // This below callback is only called when we are de-mounting the component.
+    // For every searchQuery change, component will re-render. So before rendering newly, it will unmount previous render. At that time, it will clear Timeout.
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const searchSuggestions = await fetch(API.SEARCH_SUGGESTION + searchQuery);
+
+    const json = await searchSuggestions.json();
+    setSuggestions(json[1]);
+    // console.log(json[1]);
+  };
 
   const handleToggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -24,14 +51,35 @@ const Header = () => {
         />
       </div>
       <div className="col-span-10 px-10">
-        <input
-          type="text"
-          placeholder="Search Here..!!"
-          className="w-3/4 border border-gray-400 p-2 rounded-l-full"
-        />
-        <button className="w-1/4 border border-gray-400 p-2 rounded-r-full bg-gray-100">
-          Search
-        </button>
+        <div>
+          <input
+            type="text"
+            placeholder="Search Here..!!"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+            className="w-3/4 border border-gray-400 p-2 rounded-l-full"
+          />
+          <button className="w-1/4 border border-gray-400 p-2 rounded-r-full bg-gray-100">
+            Search
+          </button>
+        </div>
+        {console.log(suggestions)}
+        {suggestions.length > 0 && showSuggestions && (
+          <div className="absolute bg-white py-2 px-2 w-[44rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  className="py-2 px-3 shadow-sm hover:bg-gray-100"
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,6 @@ export const API = {
   POPULAR_VIDEOS:
     "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
     process.env.REACT_APP_API_KEY,
+  SEARCH_SUGGESTION:
+    "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=",
 };
