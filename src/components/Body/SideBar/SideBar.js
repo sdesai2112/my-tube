82e276f7830be52df3761fe2d1b99c4f6ab31a95
Shraddha <sshradhadesai@gmail.com>
2025-07-13 12:34:41 +0530

    Used store value to show side bar

--- src/components/Body/SideBar/SideBar.js
+++ src/components/Body/SideBar/SideBar.js
@@ -1,27 +1,33 @@
+import { useSelector } from "react-redux";
+
 const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
   return (
-    <div className="shadow-lg p-5 w-48">
-      <ul>
-        <li>Home</li>
-        <li>Shorts</li>
-        <li>Videos</li>
-        <li>Live</li>
-      </ul>
-      <h1 className="font-bold pt-5">Subscriptions</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-      <h1 className="font-bold pt-5">Watch Later</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-    </div>
+    isMenuOpen && (
+      <div className="shadow-lg p-5 w-48">
+        <ul>
+          <li>Home</li>
+          <li>Shorts</li>
+          <li>Videos</li>
+          <li>Live</li>
+        </ul>
+        <h1 className="font-bold pt-5">Subscriptions</h1>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+        <h1 className="font-bold pt-5">Watch Later</h1>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+      </div>
+    )
   );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../../utils/store/slices/appSlice";
+
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleToggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
-          className="h-8"
+          onClick={handleToggleMenuHandler}
+          className="h-8 cursor-pointer"
           alt="Menu"
           src="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-4.png"
         />
