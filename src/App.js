4a4e901fd09aedefca1443bcdcd444e4daf22644
Shraddha <sshradhadesai@gmail.com>
2025-07-13 12:26:58 +0530

    Created Redux Store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body/Body";
 import Header from "./components/Header/Header";
+import store from "./utils/store/appStore";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Body />
+      <Provider store={store}>
+        <Header />
+        <Body />
+      </Provider>
     </div>
   );
 }
