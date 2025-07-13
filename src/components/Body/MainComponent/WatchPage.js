e26376947fe26cc6868e248a70f9705fcb124a39
Shraddha <sshradhadesai@gmail.com>
2025-07-13 15:39:05 +0530

    Fixes

--- src/components/Body/MainComponent/WatchPage.js
+++ src/components/Body/MainComponent/WatchPage.js
@@ -1,15 +1,30 @@
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../../../utils/store/slices/appSlice";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+
+  console.log(searchParams.get("v"));
 
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
 
-  return <div>WatchPage</div>;
+  return (
+    <div className="px-5">
+      <iframe
+        width="1000"
+        height="400"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
